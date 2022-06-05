15e8bd8cf778bb1d3fb5a5feedb544ba6f43e6f7
Jared <jared.wilson4332@gmail.com>
2022-06-05 17:09:35 -0600

    Switch function location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hi')
+import buildHeader from "./modules/header.js"
+
+export default function createThing (name, type, text, group) {
+    name = document.createElement(type)
+    name.setAttribute("id", name)
+    if (text != 0) { name.textContent = text }
+    if (group != 0) { name.classList.add(group) }
+    content.appendChild(name)
+}
+
+buildHeader()
