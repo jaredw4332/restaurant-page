1de68be7062256b42642a1f0eebed1bacb495cf8
Jared <jared.wilson4332@gmail.com>
2022-06-05 21:00:01 -0600

    Add restaurant container and button container

--- src/modules/home.js
+++ src/modules/home.js
@@ -8,8 +8,20 @@ export default function buildHome() {
     homeContainer.setAttribute("id", "homeContainer")
     content.appendChild(homeContainer)
 
-    const restaurantName = createThing("restaurantNameHome", "h1", "Mmm Borgor Classic Eatery", "restaurantName", homeContainer)
+    const restaurantNameContainer = document.createElement('div')
+    restaurantNameContainer.classList.add("restaurantNameContainer")
+    restaurantNameContainer.setAttribute("id", "restaurantNameContainerHome")
+    homeContainer.appendChild(restaurantNameContainer)
+
+    const restaurantNameTop = createThing("restaurantNameSidebarTop", "h1", "Mmm Borgor", "restaurantNameTop", restaurantNameContainer)
+    const restaurantNameBottom = createThing("restaurantNameSidebarBottom", "h1", "Classic Eatery", "restaurantNameBottom", restaurantNameContainer)
+
     const slogan = createThing("slogan", "p", "You're already full", 0, homeContainer)
-    const pickupButton = createThing("pickupButton", "button", "Order Pickup", "orderButtons", homeContainer)
-    const deliveryButton = createThing("deliveryButton", "button", "Order Delivery", "orderButtons", homeContainer)
+
+    const buttonContainer = document.createElement('div')
+    buttonContainer.setAttribute("id", "homeButtonContainer")
+    homeContainer.appendChild(buttonContainer)
+
+    const pickupButton = createThing("pickupButton", "button", "Order Pickup", "orderButtons", buttonContainer)
+    const deliveryButton = createThing("deliveryButton", "button", "Order Delivery", "orderButtons", buttonContainer)
 }
