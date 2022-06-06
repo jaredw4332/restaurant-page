3f73cb8b921ec0debdde587a389f93c609629646
Jared <jared.wilson4332@gmail.com>
2022-06-05 18:26:48 -0600

    Create elements

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,1 +1,15 @@
 import createThing from "./../index.js"
+
+export default function buildHome() {
+    
+    const content = document.getElementById("content")
+
+    const homeContainer = document.createElement('div')
+    homeContainer.setAttribute("id", "homeContainer")
+    content.appendChild(homeContainer)
+
+    const restaurantName = createThing("restaurantNameHome", "h1", "Mmm Borgor Classic Eatery", "restaurantName", homeContainer)
+    const slogan = createThing("slogan", "p", "You're already full", 0, homeContainer)
+    const pickupButton = createThing("pickupButton", "button", "Order Pickup", "orderButtons", homeContainer)
+    const deliveryButton = createThing("deliveryButton", "button", "Order Delivery", "orderButtons", homeContainer)
+}
