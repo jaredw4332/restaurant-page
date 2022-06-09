4f3eb5a2931e720883fb3b0293a3a22aab1cab78
Jared <jared.wilson4332@gmail.com>
2022-06-08 20:46:50 -0600

    Create function which loads each page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css'
-import buildSidebar from "./modules/sidebar.js"
+import buildHeader from "./modules/header.js"
 import buildHome from "./modules/home.js"
+import buildMenu from "./modules/menu.js"
 
 export default function createThing (name, type, text, group, parent) {
     let id = name
@@ -11,5 +12,44 @@ export default function createThing (name, type, text, group, parent) {
     parent.appendChild(name)
 }
 
-buildSidebar()
-buildHome()
+const page = (() => {
+
+    const content = document.getElementById("content")
+
+    function loadHome () {
+        getValues()
+        content.removeChild(menuContainer)
+        document.querySelector("#homeButton").disabled = true
+        document.querySelector("#menuButton").disabled = false
+        buildHome()
+    }
+
+    function loadMenu () {
+        getValues()
+        content.removeChild(homeContainer)
+        document.querySelector("#homeButton").disabled = false
+        document.querySelector("#menuButton").disabled = true
+        buildMenu()
+    }
+
+    function addButtonEvents(){
+        getValues()
+        homeButton.addEventListener("click", loadHome)
+        menuButton.addEventListener("click", loadMenu)
+    }
+
+    function getValues() {
+        let menuContainer = document.getElementById("menuContainer")
+        let homeContainer = document.getElementById("homeContainer")
+    
+        let homeButton = document.getElementById("homeButton")
+        let menuButton = document.getElementById("menuButton")
+    }
+
+
+    buildHeader()
+    buildHome()
+    addButtonEvents()
+
+
+})()
