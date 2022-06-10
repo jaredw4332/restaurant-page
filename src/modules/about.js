c6fa8315669496f85c4aed3bbae4f60953489aea
Jared <jared.wilson4332@gmail.com>
2022-06-09 20:59:47 -0600

    Add bio and hours

--- src/modules/about.js
+++ src/modules/about.js
@@ -6,4 +6,22 @@ export default function buildAbout() {
     const aboutContainer = document.createElement('div')
     aboutContainer.setAttribute("id", "aboutContainer")
     content.appendChild(aboutContainer)
-}
+
+    const aboutBioContainer = document.createElement('div')
+    aboutBioContainer.setAttribute("id", "aboutBioContainer")
+    aboutContainer.appendChild(aboutBioContainer)
+
+    const aboutBioHeader = createThing("aboutBioHeader", "h2", "Our Story", "aboutHeader", aboutBioContainer)
+
+    const aboutBioTop = createThing("aboutBioTop", "p", "Mmm Burger was established in 1982, by Henry M.M.M. Borgor, who believed that all the residents of Fakecity should have access to a classic eatery experience.", "aboutBio", aboutBioContainer)
+    const aboutBioBottom = createThing("aboutBioBottom", "p", "Since then, we've maintained that the most important thing about preparing food is the knowledge that people might eat it, so we strive to do our very best.", "aboutBio", aboutBioContainer)
+
+    const aboutHoursContainer = document.createElement('div')
+    aboutHoursContainer.setAttribute("id", "aboutHoursContainer")
+    aboutContainer.appendChild(aboutHoursContainer)
+
+    const aboutHoursHeader = createThing("aboutHoursHeader", "h2", "Hours", "aboutHeader", aboutHoursContainer)
+    const aboutHoursOne = createThing("aboutHoursOne", "p", "Mon-Wed: 7:00AM - 8:00PM", "aboutHours", aboutHoursContainer)
+    const aboutHoursTwo = createThing("aboutHoursTwo", "p", "Thurs-Sat: 7:00AM - 9:30PM", "aboutHours", aboutHoursContainer)
+    const aboutHoursThree = createThing("aboutHoursThree", "p", "Sunday: Closed", "aboutHours", aboutHoursContainer)
+}   
